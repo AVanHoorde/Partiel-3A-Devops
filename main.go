16d3fcca4146633dd658f64edc87366c41083807
Alexis Van Hoorde <a.vanhoorde@etu-webschoolfactory.fr>
2018-12-04 12:09:24 +0100

    repare main

--- main.go
+++ main.go
@@ -1,20 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
-
+	"os"
 	"wsf/Partiel-Devops/3A-Devops/handler"
 )
 
 func main() {
 
+	connection := os.Getenv("DATABASE_URL")
+
+	fmt.Println(connection)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	myHandler := handler.NewHandler()
 
 	server := &http.Server{
-		Addr:    ":8080",
+		Addr:    ":" + port,
 		Handler: myHandler,
+		// ReadTimeout:  10 * time.Second,
+		// WriteTimeout: 10 * time.Second,
 	}
+
+	fmt.Println("Server is running at", port)
 
 	log.Fatal(server.ListenAndServe())
 }
